f9a03807ba2fff8252a99963e3556c9d98725f7e
OIgboerika <ignoerikaonochie121@gmail.com>
2025-11-04 16:42:03 +0200

    Point hero to actual public/landing-page-images filenames

--- src/App.tsx
+++ src/App.tsx
@@ -53,12 +53,12 @@ function Home() {
         {/* Stacked showcase (images) */}
         <div className="mt-14 md:mt-20">
           <div className="relative mx-auto max-w-5xl h-80 md:h-96">
-            {/* NOTE: Place your images in public/landing-page-images/ and update names if needed */}
-            <img src="/landing-page-images/1.jpg" alt="Showcase 1" className="absolute left-4 top-8 w-40 md:w-56 aspect-[4/3] object-cover rounded-2xl shadow-xl rotate-[-6deg]" />
-            <img src="/landing-page-images/2.jpg" alt="Showcase 2" className="absolute left-36 md:left-56 top-2 w-44 md:w-64 aspect-[4/3] object-cover rounded-2xl shadow-xl rotate-[4deg]" />
-            <img src="/landing-page-images/3.jpg" alt="Showcase 3" className="absolute left-64 md:left-[26rem] top-10 w-40 md:w-56 aspect-[4/3] object-cover rounded-2xl shadow-xl rotate-[-2deg]" />
-            <img src="/landing-page-images/4.jpg" alt="Showcase 4" className="absolute left-12 md:left-24 top-40 md:top-48 w-44 md:w-64 aspect-[4/3] object-cover rounded-2xl shadow-xl rotate-[3deg]" />
-            <img src="/landing-page-images/5.jpg" alt="Showcase 5" className="absolute left-60 md:left-[22rem] top-48 md:top-56 w-40 md:w-56 aspect-[4/3] object-cover rounded-2xl shadow-xl rotate-[-5deg]" />
+            {/* Images served from /public/landing-page-images/ */}
+            <img src="/landing-page-images/abazie-uchenna-fedE6gJVSRA-unsplash.jpg" alt="Showcase 1" className="absolute left-4 top-8 w-40 md:w-56 aspect-[4/3] object-cover rounded-2xl shadow-xl rotate-[-6deg]" />
+            <img src="/landing-page-images/eyo-archibong-RJo9OowwG4g-unsplash.jpg" alt="Showcase 2" className="absolute left-36 md:left-56 top-2 w-44 md:w-64 aspect-[4/3] object-cover rounded-2xl shadow-xl rotate-[4deg]" />
+            <img src="/landing-page-images/joshua-duneebon-G8fEocK6g4w-unsplash.jpg" alt="Showcase 3" className="absolute left-64 md:left-[26rem] top-10 w-40 md:w-56 aspect-[4/3] object-cover rounded-2xl shadow-xl rotate-[-2deg]" />
+            <img src="/landing-page-images/lisa-marie-theck-igdYA2SwV8E-unsplash.jpg" alt="Showcase 4" className="absolute left-12 md:left-24 top-40 md:top-48 w-44 md:w-64 aspect-[4/3] object-cover rounded-2xl shadow-xl rotate-[3deg]" />
+            <img src="/landing-page-images/muhammad-taha-ibrahim-HfC8mUqulJ8-unsplash.jpg" alt="Showcase 5" className="absolute left-60 md:left-[22rem] top-48 md:top-56 w-40 md:w-56 aspect-[4/3] object-cover rounded-2xl shadow-xl rotate-[-5deg]" />
           </div>
         </div>
       </div>
